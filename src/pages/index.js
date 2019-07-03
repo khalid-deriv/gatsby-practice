672f6b04741b9002a11fe2c9f36925b77c76a97c
khalidxx <fxtryio@gmail.com>
2019-07-03 13:04:15 +0800

    added layout and basic graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
+import Layout from "../components/layout"
 
-const Home = () => {
-    return (
-        <h1>Hello People!</h1>
-        
-    )
-}
-
-export default Home;
+export default () => (
+  <Layout>
+    <h1>Amazing Pandas Eating Things</h1>
+    <div>
+      <img
+        src="https://2.bp.blogspot.com/-BMP2l6Hwvp4/TiAxeGx4CTI/AAAAAAAAD_M/XlC_mY3SoEw/s1600/panda-group-eating-bamboo.jpg"
+        alt="Group of pandas eating bamboo"
+      />
+    </div>
+  </Layout>
+)
